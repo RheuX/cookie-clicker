0959a6dde2665512df20b68c39a4a5ffeb0c9d9f
Honghui-Li-8 <honghuili8@gmail.com>
2023-09-27 23:02:00 -0700

    10 click ranom teleport

--- src/components/CookieButton.js
+++ src/components/CookieButton.js
@@ -72,12 +72,58 @@ function CookieButton(props) {
     props.onClick();
   };
 
+  const handleClick2 = () => {
+    setClickCount((clickCount) => clickCount + 1);
+
+    // Call the parent's onClick function to update the score
+
+    // Get a reference to the .flex-container element
+    var flexContainer = document.getElementById("flex-container");
+
+    // Get the width and height of the .flex-container
+    var containerWidth = flexContainer.offsetWidth;
+    var containerHeight = flexContainer.offsetHeight;
+
+    // Log the dimensions (for example)
+    console.log(
+      "Container Width:",
+      containerWidth,
+      "Container Height:",
+      containerHeight
+    );
+
+    //====================================================
+    if (clickCount >= 9) {
+      var button = document.getElementById("cookie-2");
+      var randomX = 0.15 + Math.random() * 0.6;
+      var randomY = 0.1 + Math.random() * 0.8;
+      randomX = randomX.toFixed(2);
+      randomY = randomY.toFixed(2);
+
+      console.log(randomX);
+      button.style.top = randomX * 100 + "%";
+      button.style.left = randomY * 100 + "%";
+
+      console.log(button.style.top);
+      setClickCount(0);
+    }
+    //====================================================
+
+    props.onClick();
+  };
+
   return (
     <div id="flex-container" style={containerStyle}>
       <button
         id="cookie"
         style={cookieButtonStyle}
         onClick={handleClick}
+      ></button>
+
+      <button
+        id="cookie-2"
+        style={cookieButtonStyle}
+        onClick={handleClick2}
       ></button>
     </div>
   );
