3ae897d4b2d069e218ef278dc70252ccfdecf3af
RheuX <axelnlorens@gmail.com>
2024-01-09 22:47:14 -0800

    Befoere Big Changes UI

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,6 +14,9 @@ const footerStyle = {
   margin: "0 20px",
 };
 
+// Wrap CookieMonster and CookieJar on something called CookieMonJar so that 
+// - i can make that 100% based on the screen, and cookie monster can just follow that
+// - now i have the size of the cookieMonster since the wrap is the cookieMonster, and now can change size based on the current wrap
 function Footer(props) {
   return (
     <div style={footerStyle}>
